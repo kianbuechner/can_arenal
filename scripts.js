c64438b648cae17e4ef697806fbde40e92edf74c
kianbuechner <83275558+kianbuechner@users.noreply.github.com>
2024-06-07 00:40:47 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Danke für Ihre Nachricht! Wir werden uns in Kürze bei Ihnen melden.');
+});
